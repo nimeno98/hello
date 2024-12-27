88030c036d8527ca73c8eae24c7ff2be2951877b
haha <username@users.noreply.github.com>
2024-12-27 17:46:41 +0800

    Step 8

---
+++ listings/choices.py
@@ -0,0 +1,48 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+}
+
+price_choices = {
+    '100000': '$1,000,000',
+    '200000': '$2,000,000',
+    '300000': '$3,000,000',
+    '400000': '$4,000,000',
+    '500000': '$5,000,000',
+    '600000': '$6,000,000',
+    '700000': '$7,000,000',
+    '800000': '$8,000,000',
+    '900000': '$9,000,000',
+    '1000000': '$10M+',
+}
+
+district_choices = {
+    '_': 'All Districts',
+    'Islands': 'Islands',
+    'Kwai_Tsing': 'Kwai Tsing',
+    'North': 'North',
+    'Sai_Kung': 'Sai Kung',
+    'Sha_Tin': 'Sha Tin',
+    'Tai_Po': 'Tai Po',
+    'Tsuen_Wan': 'Tsuen Wan',
+    'Tuen_Mun': 'Tuen Mun',
+    'Yuen_Long': 'Yuen Long',
+    'Kowloon_City': 'Kowloon City',
+    'Kwun_Tong': 'Kwun Tong',
+    'Sham_Shui_Po': 'Sham Shui Po',
+    'Wong_Tai_Sin': 'Wong Tai Sin',
+    'Yau_Tsim_Mong': 'Yau Tsim Mong',
+    'Central_&_Western': 'Central & Western',
+    'Eastern': 'Eastern',
+    'Southern': 'Southern',
+    'Wan_Chai': 'Wan Chai',
+}
+
--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import bedroom_choices, price_choices, district_choices
 
 # Create your views here.
 def index(request):
@@ -17,8 +18,52 @@ def index(request):
     return render( request, 'listings/listings.html', context )
 
 def listing( request, listing_id ):
-    return render( request, 'listings/listing.html' )
+    listing = get_object_or_404( Listing, pk = listing_id )
+    context = { 'listing': listing }
+
+    return render( request, 'listings/listing.html', context )
 
 def search( request ):
-    return render( request, 'listings/search.html' )
+    queryset_list = Listing.objects.order_by( '-list_date' )
+
+    if 'keywords' in request.GET:
+        keywords = request.GET[ 'keywords' ]
+        
+        if keywords:
+            queryset_list = queryset_list.filter( description__icontains = keywords )
+
+    if 'title' in request.GET:
+        title = request.GET[ 'title' ]
+        
+        if title:
+            queryset_list = queryset_list.filter( title__icontains = title )
+
+    if 'district' in request.GET:
+        district = request.GET[ 'district' ]
+        
+        if district:
+            if district != '_':
+                queryset_list = queryset_list.filter( district__iexact = district )
+
+    if 'price' in request.GET:
+        price = request.GET[ 'price' ]
+        
+        if price:
+            queryset_list = queryset_list.filter( price__lte = price )
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET[ 'bedrooms' ]
+        
+        if bedrooms:
+            queryset_list = queryset_list.filter( bedrooms__lte = bedrooms )
+
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'district_choices': district_choices,
+        'listings': queryset_list,
+        'values': request.GET,
+    }
+
+    return render( request, 'listings/search.html', context )
 
