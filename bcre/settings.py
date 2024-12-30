60d8afd8059b465f5333bb3a1a44ed0f511f1b78
haha <username@users.noreply.github.com>
2024-12-30 17:41:51 +0800

    Step 9

--- bcre/settings.py
+++ bcre/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,3 +150,9 @@ INTERNAL_IPS = [
     # 'localhost',
     # ...
 ]
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,8 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+// fix later
+
+setTimeout(() => {
+    $("#message").fadeOut("slow");
+}, 3000);
